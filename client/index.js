ab08fbc3f0a3d2351d27f9bad6827024dc765f35
notyourbear <pracenberg@gmail.com>
2017-01-22 12:26:58 -0800

    unroutered

--- client/index.js
+++ client/index.js
@@ -2,15 +2,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-// Import css
-// import css from './styles/style.styl'
-
 import App from './component/App';
 import Game from './component/Game';
-import Start from './component/Start';
-// import Single from './components/Single';
-// import PhotoGrid from './components/PhotoGrid';
-//
+
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 
@@ -20,8 +14,7 @@ const router = (
 	<Provider store={store}>
 		<Router history={history}>
 			<Route path="/" component={App}>
-				<IndexRoute component={Start}></IndexRoute>
-				<Route path='/game' component={Game}></Route>
+				<IndexRoute component={Game}></IndexRoute>
 			</Route>
 		</Router>
 	</Provider>
